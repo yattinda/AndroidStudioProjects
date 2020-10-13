12de7ad2c87d82f0259e2adb2b7814b4b4361d86
yattinda <yattinda@gmail.com>
2020-10-13 15:42:16 +0900

    Copy tempalte

---
+++ template/lib/root.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'routes/home.dart';
+import 'routes/talk.dart';
+import 'routes/numbergirl.dart';
+
+class RootWidget extends StatefulWidget {
+  RootWidget({Key key}) : super(key: key);
+
+  @override
+  _RootWidgetState createState() => _RootWidgetState();
+}
+
+class _RootWidgetState extends State<RootWidget> {
+  int _selectedIndex = 0;
+  final _bottomNavigationBarItems = <BottomNavigationBarItem>[];
+
+  static const _footerIcons = [
+    Icons.home,
+    Icons.textsms,
+    Icons.ramen_dining,
+  ];
+
+  static const _footerItemNames = [
+    'ホーム',
+    'トーク',
+    'NumberGirl',
+  ];
+
+  var _routes = [
+    Home(),
+    Talk(),
+    NumberGirl(),
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    _bottomNavigationBarItems.add(_UpdateActiveState(0));
+    for (var i = 1; i < _footerItemNames.length; i++) {
+      _bottomNavigationBarItems.add(_UpdateDeactiveState(i));
+    }
+  }
+
+  /// activate
+  BottomNavigationBarItem _UpdateActiveState(int index) {
+    return BottomNavigationBarItem(
+        icon: Icon(
+          _footerIcons[index],
+          color: Colors.black87,
+        ),
+        title: Text(
+          _footerItemNames[index],
+          style: TextStyle(
+            color: Colors.black87,
+          ),
+        )
+    );
+  }
+
+  BottomNavigationBarItem _UpdateDeactiveState(int index) {
+    return BottomNavigationBarItem(
+        icon: Icon(
+          _footerIcons[index],
+          color: Colors.black26,
+        ),
+        title: Text(
+          _footerItemNames[index],
+          style: TextStyle(
+            color: Colors.black26,
+          ),
+        )
+    );
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _bottomNavigationBarItems[_selectedIndex] =
+          _UpdateDeactiveState(_selectedIndex);
+      _bottomNavigationBarItems[index] = _UpdateActiveState(index);
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _routes.elementAt(_selectedIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: _bottomNavigationBarItems,
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
---
+++ template/lib/routes/numbergirl.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class NumberGirl extends StatelessWidget {
+  // <- (※1)
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Numbergirl"), // <- (※2)
+      ),
+      body: Center(// <- (※3)
+          ),
+      drawer: Drawer(
+        // swipe
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text(
+                '福岡市博多区からやってまいりましたNumberGirlです',
+                style: TextStyle(color: Colors.white),
+              ),
+              decoration: BoxDecoration(
+                color: Colors.black,
+              ),
+            ),
+            ListTile(
+              title: Text('ドラムス　アヒト・イナザワ'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
